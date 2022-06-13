2719eee940244e4aec0789b184bdf0581b70d939
Michael Olbrich <m.olbrich@pengutronix.de>
2022-06-13 15:38:13 +0200

    compositor: only reflow the outputs if the shell did not move them
    weston_compositor_reflow_outputs() assumes that all output are positioned from
    left to right with no gaps in the same order in which they where created.
    
    If the shell moves an output with weston_output_move() then this assumption is
    no longer true. So stop reflowing the outputs in the case. The shell is now
    responsible for positioning all outputs as needed.
    
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- libweston/compositor.c
+++ libweston/compositor.c
@@ -5839,6 +5839,9 @@ weston_head_get_destroy_listener(struct weston_head *head,
 	return wl_signal_get(&head->destroy_signal, notify);
 }
 
+static void
+weston_output_set_position(struct weston_output *output, int x, int y);
+
 /* Move other outputs when one is resized so the space remains contiguous. */
 static void
 weston_compositor_reflow_outputs(struct weston_compositor *compositor,
@@ -5846,6 +5849,9 @@ weston_compositor_reflow_outputs(struct weston_compositor *compositor,
 {
 	struct weston_output *output;
 	bool start_resizing = false;
+
+	if (compositor->output_flow_dirty)
+		return;
 
 	if (!delta_width)
 		return;
@@ -5857,7 +5863,7 @@ weston_compositor_reflow_outputs(struct weston_compositor *compositor,
 		}
 
 		if (start_resizing) {
-			weston_output_move(output, output->x + delta_width, output->y);
+			weston_output_set_position(output, output->x + delta_width, output->y);
 			output->dirty = 1;
 		}
 	}
@@ -5951,8 +5957,8 @@ weston_output_init_geometry(struct weston_output *output, int x, int y)
 /**
  * \ingroup output
  */
-WL_EXPORT void
-weston_output_move(struct weston_output *output, int x, int y)
+static void
+weston_output_set_position(struct weston_output *output, int x, int y)
 {
 	struct weston_head *head;
 	struct wl_resource *resource;
@@ -5996,6 +6002,16 @@ weston_output_move(struct weston_output *output, int x, int y)
 			zxdg_output_v1_send_done(resource);
 		}
 	}
+}
+
+/**
+ * \ingroup output
+ */
+WL_EXPORT void
+weston_output_move(struct weston_output *output, int x, int y)
+{
+	output->compositor->output_flow_dirty = true;
+	weston_output_set_position(output, x, y);
 }
 
 /** Signal that a pending output is taken into use.
